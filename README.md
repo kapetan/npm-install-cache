4aa00088e7472ca9b8cfb3c8a789d3625ef06089
Mirza Kapetanovic <mirza.kapetanovic@gmail.com>
2015-01-19 20:30:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# npm-install-cache
+
+Install `node_modules` from cache on unix-like systems.
+
+	npm install npm-install-cache
+
+## Usage
+
+Navigate to node project and run
+
+	npm-install-cache
+
+If any changes to the `package.json` have been made since the last run the script executes `npm install` and stores a copy of the `node_modules` directory, these are used next time the script is executed.
---
+++ npm-install-cache.sh
@@ -0,0 +1,37 @@
+function get_cache_dir() {
+	local cache_id=$(cat package.json | grep '"name"' | sed 's/"name"//; s/[^"]*"//; s/"[^"]*//')
+
+	if [ "$cache_id" == "" ]; then
+		cache_id=__misc__
+	fi
+
+	echo "/tmp/npm-install-cache/$cache_id"
+}
+
+function get_hash() {
+	echo $(shasum package.json | shasum | sed 's/[ \t]*-[ \t]*//')
+}
+
+function read_hash() {
+	echo $(cat "$1/package.shasum" 2> /dev/null)
+}
+
+function build() {
+	local cache_dir=$(get_cache_dir)
+
+	mkdir -p "$cache_dir"
+
+	local new_hash=$(get_hash)
+	local old_hash=$(read_hash "$cache_dir")
+
+	if [ "$new_hash" == "$old_hash" ]; then
+		cp -r "$cache_dir/" node_modules
+	else
+		npm install || exit $?
+		rm -rf "$cache_dir"
+		cp -r "node_modules/" "$cache_dir"
+		echo "$new_hash" > "$cache_dir/package.shasum"
+	fi
+}
+
+build
