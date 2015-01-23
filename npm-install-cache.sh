0b82b5c46b4f3af240889a3ec200e8f48f1797da
Mirza Kapetanovic <mirza.kapetanovic@gmail.com>
2015-01-23 11:57:58 +0100

    Cache every package.json version

--- npm-install-cache.sh
+++ npm-install-cache.sh
@@ -1,39 +1,52 @@
 #!/bin/bash
 
-function get_cache_dir() {
-	local cache_id=$(cat package.json | grep '"name"' | sed 's/"name"//; s/[^"]*"//; s/".*//')
+DEFAULT_RETAIN=10
 
-	if [ "$cache_id" == "" ]; then
-		cache_id=__misc__
+function _retain() {
+	local r=${1:-$DEFAULT_RETAIN}
+
+	if [ "$r" == all ]; then
+		cat
+	else
+		tail -n "+$((r + 1))"
 	fi
-
-	echo "/tmp/npm-install-cache/$cache_id"
 }
 
-function get_hash() {
+function get_package_hash() {
 	echo $(cat package.json | shasum | sed 's/[ \t]*-[ \t\n]*$//')
 }
 
-function read_hash() {
-	echo $(cat "$1/package.shasum" 2> /dev/null)
+function get_cache_dir() {
+	local cache_id=$(get_package_hash)
+	echo "/tmp/npm-install-cache/$cache_id"
+}
+
+function clean_cache_dir() {
+	ls -tp /tmp/npm-install-cache | grep /$ | _retain "$1" | while read d; do
+		rm -rf "/tmp/npm-install-cache/$d"
+	done
 }
 
 function build() {
 	local cache_dir=$(get_cache_dir)
 
-	mkdir -p "$cache_dir"
-
-	local new_hash=$(get_hash)
-	local old_hash=$(read_hash "$cache_dir")
-
-	if [ "$new_hash" == "$old_hash" ]; then
+	if [ -d "$cache_dir" ]; then
 		cp -r "$cache_dir/" node_modules
 	else
 		npm install || exit $?
-		rm -rf "$cache_dir"
 		cp -r "node_modules/" "$cache_dir" 2> /dev/null || mkdir -p "$cache_dir"
-		echo -n "$new_hash" > "$cache_dir/package.shasum"
 	fi
 }
 
-build
+function usage() {
+	local dir="$(cd "$(dirname "$0")" && pwd)"
+	cat "$dir/usage.txt"
+}
+
+if [ "$1" == clean ]; then
+	clean_cache_dir "$2"
+elif [ "$1" == help ]; then
+	usage
+else
+	build
+fi
