64b0d6a2ccba74236191cd725032b39913bd8d82
Mirza Kapetanovic <mirza.kapetanovic@gmail.com>
2015-10-06 16:36:58 +0200

    Pass arguments to npm install

--- npm-install-cache.sh
+++ npm-install-cache.sh
@@ -29,11 +29,11 @@ function build() {
 	if [ "$new_hash" == "$old_hash" ]; then
 		cp -r "$cache_dir/" node_modules
 	else
-		npm install || exit $?
+		npm install "$@" || exit $?
 		rm -rf "$cache_dir"
 		cp -r "node_modules/" "$cache_dir" 2> /dev/null || mkdir -p "$cache_dir"
 		echo -n "$new_hash" > "$cache_dir/package.shasum"
 	fi
 }
 
-build
+build "$@"
